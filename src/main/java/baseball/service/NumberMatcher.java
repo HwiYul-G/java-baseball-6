db398ba0ccc02b4499ed611116d5e5857aa1ad1f
kim <hwiyul_g@daum.net>
2023-11-30 23:02:30 +0900

    refactor : 매칭기능에서 else문 제거

--- src/main/java/baseball/service/NumberMatcher.java
+++ src/main/java/baseball/service/NumberMatcher.java
@@ -7,13 +7,15 @@ public class NumberMatcher {
 
     private static final int NUMBER_SIZE = 3;
 
-    public GameResult match(List<Integer> userNumber, List<Integer> answerNumber) {
+    public GameResult match(final List<Integer> userNumber, final List<Integer> answerNumber) {
         GameResult result = new GameResult();
 
         for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (userNumber.get(i).equals(answerNumber.get(i))) {
+            boolean isStrike = userNumber.get(i).equals(answerNumber.get(i));
+            if (isStrike) {
                 result = result.incrementStrike();
-            } else if (answerNumber.contains(userNumber.get(i))) {
+            }
+            if (!isStrike && answerNumber.contains(userNumber.get(i))) {
                 result = result.incrementBall();
             }
         }
