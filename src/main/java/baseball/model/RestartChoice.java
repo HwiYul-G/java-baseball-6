d9f052e2487141c5fe4e048febb96b0380c12aaf
kim <hwiyul_g@daum.net>
2023-12-02 15:49:36 +0900

    refactor : InputView는 최소한의 입력(String)만 받고 RestartChoice라는 레코드에서 이를 검증하도록 변경

---
+++ src/main/java/baseball/model/RestartChoice.java
@@ -0,0 +1,29 @@
+package baseball.model;
+
+import baseball.utils.ExceptionMessage;
+
+public record RestartChoice(String choice) {
+
+    private static final String RESTART = "1";
+    private static final String QUIT = "2";
+
+    public RestartChoice {
+        validate(choice);
+    }
+
+    private void validate(final String choice) {
+        if (!choice.equals(QUIT) && !choice.equals(RESTART)) {
+            throw new IllegalArgumentException(
+                ExceptionMessage.INVALID_RESTART_OR_NOT_MESSAGE.getMessage());
+        }
+    }
+
+    public boolean isQuit() {
+        return choice().equals(QUIT);
+    }
+
+    public boolean isRestart() {
+        return choice().equals(RESTART);
+    }
+
+}
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,26 +4,29 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String REQUEST_USER_INPUT = "숫자를 입력해주세요 : ";
-    private static final String REQUEST_RESTART_OR_NOT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String RESTART = "1";
-    private static final String QUIT = "2";
-    private static final String INPUT_ERROR_MESSAGE = "1또는 2만 입력하세요.";
+    private enum InputConsoleMessage {
+        USER_NUMBER_INPUT("숫자를 입력해주세요 : "),
+        RESTART_OR_NOT("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        private final String message;
+
+        InputConsoleMessage(final String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
 
     public String requestUserNumber() {
-        System.out.print(REQUEST_USER_INPUT);
+        System.out.print(InputConsoleMessage.USER_NUMBER_INPUT.getMessage());
         return Console.readLine();
     }
 
-    public int requestGameRestart() {
-        System.out.println(REQUEST_RESTART_OR_NOT);
-        String userInput = Console.readLine();
-
-        if (!RESTART.equals(userInput) && !QUIT.equals(userInput)) {
-            throw new IllegalArgumentException(INPUT_ERROR_MESSAGE);
-        }
-
-        return Integer.parseInt(userInput);
+    public String requestGameRestart() {
+        System.out.println(InputConsoleMessage.RESTART_OR_NOT.getMessage());
+        return Console.readLine();
     }
 
 }
