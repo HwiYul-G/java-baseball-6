dbb0c3ba05ba918fe77c51530d4a776fbdd875fc
kim <hwiyul_g@daum.net>
2023-12-02 16:11:54 +0900

    refactor(OutputView) : ConsoleMessage enum으로 분리 및 else 안 쓰도록 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,28 +2,48 @@ package baseball.view;
 
 public class OutputView {
 
-    private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String WINNING_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private enum OutputConsoleMessage {
 
-    public void displayStartGame() {
-        System.out.println(START_GAME_MESSAGE);
-    }
+        START_GAME_MESSAGE("숫자 야구 게임을 시작합니다."),
+        WINNING_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
 
-    public void displayResult(int balls, int strikes) {
+        BALL_AND_STRIKE("%d볼 %d스트라이크\n"),
+        BALL("%d볼\n"),
+        STRIKE("%d스트라이크\n"),
+        NOTHING("낫싱\n");
 
-        if (balls > 0 && strikes > 0) {
-            System.out.printf("%d볼 %d스트라이크\n", balls, strikes);
-        } else if (balls > 0) {
-            System.out.printf("%d볼\n", balls);
-        } else if (strikes > 0) {
-            System.out.printf("%d스트라이크\n", strikes);
-        } else if (balls == 0 && strikes == 0) {
-            System.out.println("낫싱");
+        private final String message;
+
+        OutputConsoleMessage(final String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
         }
     }
 
+    public void displayStartGame() {
+        System.out.println(OutputConsoleMessage.START_GAME_MESSAGE.getMessage());
+    }
+
+    public void displayResult(int balls, int strikes) {
+        String message = OutputConsoleMessage.NOTHING.getMessage();
+        if (balls > 0 && strikes > 0) {
+            message = String.format(OutputConsoleMessage.BALL_AND_STRIKE.getMessage(), balls,
+                strikes);
+        }
+        if (balls > 0 && strikes == 0) {
+            message = String.format(OutputConsoleMessage.BALL.getMessage(), balls);
+        }
+        if (strikes > 0 && balls == 0) {
+            message = String.format(OutputConsoleMessage.STRIKE.getMessage(), strikes);
+        }
+        System.out.print(message);
+    }
+
     public void displayWinningMessage() {
-        System.out.println(WINNING_MESSAGE);
+        System.out.println(OutputConsoleMessage.WINNING_MESSAGE.getMessage());
     }
 
 }
